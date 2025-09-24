6ca1dfb3a2262725996621be26156eb673f1e0a5
BurtYoshi <139944399+BurtYoshi@users.noreply.github.com>
2025-09-24 17:14:35 -0400

    Creation of both BlackJack files and more minor edits of GuessingGame
    Renamed MathGuessingGame to NumberGuessingGame and the creation and development of both BlackJack files, easy mode inteded to be completed relatively quickly and hard mode when I fully understand stuff

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 import java.util.Random;
 
-class MathGuessingGame {
+class NumberGuessingGame {
     public static void main(String[] args) {
         //Variables to control parameters of the game
         int min = 0; //recommended to be zero, untested situations where it isn't zero
         int max = 100;
-        int guesses = ((max / 10) - 1); //make this 1 less than you actually want
+        int guesses = ((max / 15) - 1); //make this 1 less than you actually want
         int attempts = 1; //counts attempts until correct guess (do not manipulate)
-        Random rand = new Random(); //Randomizer, to make the game different every round
+        Random rand = new Random(); //Randomizer for the game answer
         int answer = rand.nextInt(100) + 1; //edit only the number in parenthesis
 
         System.out.println("Welcome to the Number Guessing Game!\n");
@@ -24,18 +24,20 @@ class MathGuessingGame {
         //Basic gameplay loop, runs through a checklist, letters break it
         while (guesses >= 0) {
             if (guesses == 0) {
-                System.out.println("You have encountered an error or ran out of guesses");
+                System.out.println("You have encountered an error or ran out of guesses.");
                 System.out.println("Try again sometime!");
                 reader.close();
-                guesses = -1;
+                guesses = -1; //prevents an infinite loop due to the condition always being met
             }
+            //When the input is not between the min and max
             else if (response >= max || response <= min) {
                 System.out.println("\nWrite a number between " + min + " and " + max);
-                guesses --;
                 attempts ++;
                 System.out.println("Guesses remaining: " + (guesses + 1));
                 response = reader.nextInt();
-            } else if (response > answer && response < max) {
+            }
+            //When the input is greater than the answer
+            else if (response > answer && response < max) {
                 max = response;
                 System.out.println("\nGuess a lower number!");
                 System.out.println("High guess: " + max);
@@ -44,7 +46,9 @@ class MathGuessingGame {
                 attempts ++;
                 System.out.println("Guesses remaining: " + (guesses + 1));
                 response = reader.nextInt();
-            } else if (response < answer && response > min) {
+            }
+            //When the input is less than the answer
+            else if (response < answer && response > min) {
                 System.out.println("\nGuess a higher number!");
                 min = response;
                 System.out.println("High guess: " + max);
@@ -53,7 +57,9 @@ class MathGuessingGame {
                 attempts ++;
                 System.out.println("Guesses remaining: " + (guesses + 1));
                 response = reader.nextInt();
-            } else if (response == answer) {
+            }
+            //When the user guesses the answer
+            else if (response == answer) {
                 System.out.println("\nYou have guessed the number!");
                 System.out.println("You took " + attempts + " attempts!");
                 guesses = -1;
