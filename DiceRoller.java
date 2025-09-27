832ba72fc499270323083e74391f10050143f53e
BurtYoshi <139944399+BurtYoshi@users.noreply.github.com>
2025-09-27 01:10:07 -0400

    Phase 1 and 2 complete
    Phase 3 planning and completion of phases 1 and 2, edits throughout the code.

--- DiceRoller.java
+++ DiceRoller.java
@@ -1,13 +1,8 @@
 import java.util.Scanner;
 /*Future Features:
 ability to roll multiple types of die at once
-dice sides defaults to 6 and number of dice defaults to 1
-ability to roll up to 100 of each dice
 an infinite game until the user ends it
-a breakdown showing the math behind the final calculation (each die type added together)
-three phases of the game, # of sides, # of dice, and calculation period
-(warn that once a period is finished, you cannot go back)
-phases do not work properly, fix to where all paths end with going to next phase*/
+a breakdown showing the math behind the final calculation (each die type added together) */
 public class DiceRoller {
     public static void main(String[] args) {
         //Declaration of the games default variables
@@ -23,8 +18,9 @@ public class DiceRoller {
         Scanner InputReader = new Scanner(System.in);
         int UserInput = InputReader.nextInt();
         while (AmountSelection) { //Start of the first phase of the game
-            if (UserInput == -1) {
-                //Write this towards the end when all conditions are created (for a loop)
+            if (UserInput == -1) { //Confirmation block, sends the code to the next loop afterwards
+                System.out.println("You have confirmed your selection, it cannot be changed");
+                AmountSelection = false;
             }
             else if (UserInput == -2) {
                 System.out.println("\nYou have chosen to end the game");
@@ -38,15 +34,18 @@ public class DiceRoller {
             else if (0 < UserInput && UserInput < 100) {
                 NumberOfDice = UserInput;
                 System.out.println("\nYou have chosen to roll " + NumberOfDice + " dice");
-                AmountSelection = false;
+                System.out.println("Type '-1' to confirm decision and -2 to end the game");
+                UserInput = InputReader.nextInt();
             }
         }
+        //Intro to the second phase
+        System.out.println("\nPhase two has been reached, selecting the number of sides on each dice");
+        System.out.println("How many sides do you want each dice to have?");
+        UserInput = InputReader.nextInt();
         while (SideSelection) { //Start of the second phase of the game
-            System.out.println("\nPhase two has been reached");
-            System.out.println("How many sides do you want each dice to have?");
-            UserInput = InputReader.nextInt();
             if (UserInput == -1) {
-                //Write this towards the end when all conditions are created (for a loop)
+                System.out.println("You have confirmed your selection, it cannot be changed");
+                SideSelection = false;
             }
             else if (UserInput == -2) {
                 System.out.println("\nYou have chosen to end the game");
@@ -59,12 +58,20 @@ public class DiceRoller {
             }
             else if (0 < UserInput && UserInput < 100) {
                 DiceSides = UserInput;
-                System.out.println("\nYou have chosen to have " + DiceSides + " dice");
-                SideSelection = false;
+                System.out.println("\nYou have chosen to have " + DiceSides + " sides on each dice");
+                System.out.println("Type '-1' to confirm decision and -2 to end the game");
+                UserInput = InputReader.nextInt();
             }
         }
-        while (CalculationTime) {
+        //Intro to the third and final phase
+        System.out.println("\nPhase three has been reached, the rolling must commence");
+        System.out.println("Tell the programmer to make the code, it does not exist");
+        System.out.println("Fourth wall break :)"); //Delete this and the proceeding line
+        UserInput = InputReader.nextInt();
+        while (CalculationTime) { //Different from first two phases, use RollTotal as final answer
+            /*if (UserInput == -1)*/ {
 
+            }
         }
     }
 }
