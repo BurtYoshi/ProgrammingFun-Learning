2b9ea11de140fbd3752be67292a86b24c97fa4c4
BurtYoshi <139944399+BurtYoshi@users.noreply.github.com>
2025-09-26 22:35:48 -0400

    DiceRoller phase work (1 and 2)
    Neither phase work properly but most of the code is in place

--- DiceRoller.java
+++ DiceRoller.java
@@ -6,34 +6,65 @@ ability to roll up to 100 of each dice
 an infinite game until the user ends it
 a breakdown showing the math behind the final calculation (each die type added together)
 three phases of the game, # of sides, # of dice, and calculation period
-(warn that once a period is finished, you cannot go back) */
+(warn that once a period is finished, you cannot go back)
+phases do not work properly, fix to where all paths end with going to next phase*/
 public class DiceRoller {
     public static void main(String[] args) {
         //Declaration of the games default variables
+        boolean AmountSelection = true; //Three booleans are phases of the game
         boolean SideSelection = true;
-        int DiceNumber = 3;
-        int DiceSides = 6;
+        boolean CalculationTime = true;
+        int NumberOfDice;
+        int DiceSides;
+        int RollTotal;
 
         System.out.println("This is a basic dice roller machine, welcome :)");
         System.out.println("How many dice do you want to roll? (maximum of 100)");
         Scanner InputReader = new Scanner(System.in);
         int UserInput = InputReader.nextInt();
-        while (SideSelection) {
+        while (AmountSelection) { //Start of the first phase of the game
             if (UserInput == -1) {
-                //Write this towards the end when all conditions are created
+                //Write this towards the end when all conditions are created (for a loop)
             }
             else if (UserInput == -2) {
-                System.out.println("You have chosen to end the game");
+                System.out.println("\nYou have chosen to end the game");
                 System.out.println("Thank you for playing!");
-                gameactive = false;
+                System.exit(0);
             }
             else if (UserInput > 100 || UserInput < 0) {
-                System.out.println("Please type an integer between 1 and 100");
+                System.out.println("\nPlease type an integer between 1 and 100");
                 UserInput = InputReader.nextInt();
             }
-            else if () {
-
+            else if (0 < UserInput && UserInput < 100) {
+                NumberOfDice = UserInput;
+                System.out.println("\nYou have chosen to roll " + NumberOfDice + " dice");
+                AmountSelection = false;
             }
+        }
+        while (SideSelection) { //Start of the second phase of the game
+            System.out.println("\nPhase two has been reached");
+            System.out.println("How many sides do you want each dice to have?");
+            UserInput = InputReader.nextInt();
+            if (UserInput == -1) {
+                //Write this towards the end when all conditions are created (for a loop)
+            }
+            else if (UserInput == -2) {
+                System.out.println("\nYou have chosen to end the game");
+                System.out.println("Thank you for playing!");
+                System.exit(0);
+            }
+            else if (UserInput > 100 || UserInput < 0) {
+                System.out.println("\nPlease type an integer between 1 and 100");
+                UserInput = InputReader.nextInt();
+            }
+            else if (0 < UserInput && UserInput < 100) {
+                DiceSides = UserInput;
+                System.out.println("\nYou have chosen to have " + DiceSides + " dice");
+                SideSelection = false;
+            }
+        }
+        while (CalculationTime) {
+
         }
     }
 }
