6fc421e73d154f17ce4944ac40925b698b44ef21
BurtYoshi <139944399+BurtYoshi@users.noreply.github.com>
2025-10-21 21:07:12 -0400

    restoration of RPS.java from corrupted state

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -8,16 +8,38 @@ public class RockPaperScissors {
     public static void main(String[] args) {
         //Initial setup of the game
         System.out.println("Welcome to the game, a game as old as time...");
-        System.out.println("Rock, Paper, Scissors!");
+        System.out.println("Rock(1), Paper(2), Scissors(3)!");
         System.out.println("Choose a move.");
-
         Scanner InputReader = new Scanner(System.in);
-        String UserMove = InputReader.nextLine();
-
-
+        int UserInput = InputReader.nextInt();
+        boolean Continue = true;
+        boolean Results = true;
         Random NumberGenerator = new Random();
-        int ComputerMove = NumberGenerator.nextInt(3) + 1; //prevents a zero being returned
 
-        InputReader.close(); //Apparently a resource risk when not closed
+        while (Continue) { //loop to ensure the player does not continue before choosing a move
+            if (UserInput > 0 && UserInput < 4) {
+                //Creates the player and computer move variables
+                String ComputerMove;
+                String PlayerMove;
+                String[] Moves = {"Rock", "Paper", "Scissors"};
+
+                //Determines and stores the player and computer's moves
+                PlayerMove = Moves[UserInput - 1];
+                System.out.println("You have chosen: " + PlayerMove);
+                ComputerMove = Moves[NumberGenerator.nextInt(3)];
+                System.out.println("The computer has chosen: " + ComputerMove);
+
+                Continue = false;
+            } else if (UserInput >= 4 || UserInput <= 0){
+                System.out.println("Type a number between 1 and 3!");
+                UserInput = InputReader.nextInt();
+            } else {
+                System.out.println("You have found a new error, please report this");
+            }
+        }
+        while (Results) {
+            System.out.println("Delete this later bud");
+        }
+        InputReader.close();
     }
 }
