2449c21cb854fa7ab2c3077746c146f231e21f9f
BurtYoshi <139944399+BurtYoshi@users.noreply.github.com>
2025-10-23 21:44:12 -0400

    Phase 1 BlackJack.java
    Base functionality of the game done, game rules need to be added later

--- BlackJack.java
+++ BlackJack.java
@@ -1,18 +1,22 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*; //Simplifies 4 imports into 1 import
 
 public class BlackJack {
-        //Set up for creation of player and computer's hands
+    //Set up for creation of player and computer's hands
     public static void main(String[] args) {
-        //creation of the arrays for the deck, need to change these to lists later
+        //creation of the arrays for the deck
         ArrayList<String> suit = new ArrayList<>(Arrays.asList("Diamonds", "Hearts", "Clubs", "Spades"));
         ArrayList<String> rank = new ArrayList<>(Arrays.asList("Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"));
         ArrayList<String> deck = new ArrayList<>();
+        String UserInput;
+        boolean PlayerDrawing = true;
+        boolean ComputerDraw = true;
+        boolean Endgame = true;
+        Scanner InputReader = new Scanner(System.in);
+
         for (String suits : suit) {
             for (String ranks : rank) {
                 deck.add(ranks + " of " + suits);
-                //System.out.println(ranks + " of " + suits);
+                //System.out.println(ranks + " of " + suits); tests creation of deck, not needed
             }
         }
         //Set up for creation of the player and computer's hands
@@ -21,20 +25,39 @@ public class BlackJack {
         ArrayList<String> ComputerHand = new ArrayList<>();
         int RandomIndex;
         String DrawnCard;
-        //Creation of player and computer hand, going to be changed, this is just to make sure drawing is correct
-        for (int i = 0; i < 5; i++) {
-            RandomIndex = CardDrawer.nextInt(deck.size());
-            DrawnCard = deck.remove(RandomIndex);
-            PlayerHand.add(DrawnCard);
+
+        //START OF THE ACTUAL GAME
+        while (PlayerDrawing) { //responses do not work
+            System.out.println("Hit or Stand");
+            UserInput = InputReader.nextLine();
+            //Draws card when "hit", ends when "stand"
+            if (UserInput.equals("Hit") || UserInput.equals("hit")) {
+                for (int i = 0; i < 1; i++) {
+                    RandomIndex = CardDrawer.nextInt(deck.size());
+                    DrawnCard = deck.remove(RandomIndex);
+                    PlayerHand.add(DrawnCard);
+                    System.out.println("New card: " + DrawnCard);
+                    System.out.println("Current deck: " + PlayerHand);
+                }
+            } else if (UserInput.equals("Stand") || UserInput.equals("stand")) {
+                PlayerDrawing = false;
+            } else {
+                PlayerDrawing = false;
+            }
         }
-        for (int i = 0; i < 5; i++) {
-            RandomIndex = CardDrawer.nextInt(deck.size());
-            DrawnCard = deck.remove(RandomIndex);
-            ComputerHand.add(DrawnCard);
+        while (ComputerDraw) {
+            for (int i = 0; i < 3; i++) { //Draws computer hand
+                RandomIndex = CardDrawer.nextInt(deck.size());
+                DrawnCard = deck.remove(RandomIndex);
+                ComputerHand.add(DrawnCard);
+            }
+            ComputerDraw = false;
         }
-        System.out.println("Computer's current hand: " + ComputerHand);
-        System.out.println("Player's current hand: " + PlayerHand);
-        System.out.println("Computer's current hand: " + ComputerHand);
-        System.out.println("Player's current hand: " + PlayerHand);
+        while (Endgame) {
+            System.out.println("Computer's final hand: " + ComputerHand);
+            System.out.println("Player's final hand: " + PlayerHand);
+            System.out.println("You have won/lost!");
+            Endgame = false;
+        }
     }
 }
--- out/production/ProgrammingFun-Learning/README.md
+++ out/production/ProgrammingFun-Learning/README.md
@@ -1,9 +1,10 @@
 A collection of projects that I am making to learn coding basics when bored during my programming class.
- Might be more formal at a later date but it's literally just make this basic game and stick it in the repository.
+Might be more formal at a later date but it's literally just make this basic game and stick it in the repository.
  
-<<<<<<< HEAD
- Current dev. plan: Starting work on RPS and BJ, will decide which route to go from there. (Likely will be BJ)
- Future plans: (DR) allow decision to be edited rather than just ending the game. allow the game to be infinite, \n before last printed line
-=======
- Current dev. plan: Starting work on RPS and BJ, will decide which route to go from there. (Likely will be BJ)
->>>>>>> 0f43e0336e4cbfc97ec50df9a0bf73d48cfe9ddb
+Current dev. plan: Final revisions to the DR game then decide between RPS and BJ which to be completed first
+
+Future features to add: 
+
+ (RPS) Infinite game, score tracking wins and losses, calculate winner of each round,
+
+ (BJ) How to score the cards (index #?), draw cards when prompted by the user and decide score in real time, basic game rules coded
