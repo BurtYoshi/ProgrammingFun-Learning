36a19440d1212ab12490d1e2e4d4f7ebf8ec0822
BurtYoshi <BurtTheYoshi@gmail.com>
2026-02-13 00:32:32 -0500

    Numerical Algorithms Completed, Removal of Unnecessary Files
    Looping does not work properly, will be fixed in the next version

--- AlgorithmChallenge.java
+++ AlgorithmChallenge.java
@@ -1,8 +1,8 @@
 import java.util.*;
-//SelectionTime appears to be running properly, moving onto numerical algorithms
+//add if statement to see if input is valid first then print instructions
+//while loop does not properly loop, question is asked though
 public class AlgorithmChallenge {
     public static void main(String[] args) {
-        //declaration of game variables
         int UserInput;
         String UserString;
         boolean SelectionTime = true;
@@ -10,66 +10,87 @@ public class AlgorithmChallenge {
         boolean EvenTime = false;
         boolean VowelTime = false;
         boolean ReverseTime = false;
-        //rest of boolean for other algorithms, all set to false
+        boolean Gaming = true;
         Scanner InputReader = new Scanner(System.in);
 
         System.out.println("Welcome to the Algorithm Challenge!\n");
-        System.out.println("1. Find Factors\n2. Find even digits");
-        System.out.println("3. Hide vowels\n4. Reverse string");
+        System.out.println("1. Find Factors\n2. Find Even Digits");
+        System.out.println("3. Hide Vowels\n4. Reverse String");
         System.out.print("\nChoose a challenge from the list above: ");
         UserInput = InputReader.nextInt();
 
-        while (SelectionTime) {
-            //loop section of phase 1
-            if (UserInput < 0 || UserInput > 4) {
-                System.out.println("You have ended the game");
-                System.out.println("Thank you for playing!");
-                break;
-            } else if (UserInput == 1) {
-                System.out.println("Find factors has been chosen");
-                FactorTime = true;
-                break;
-            } else if (UserInput == 2) {
-                System.out.println("Find even digits has been chosen");
-                EvenTime = true;
-                break;
-            } else if (UserInput == 3) {
-                System.out.println("Hide vowels has been chosen");
-                VowelTime = true;
-                break;
-            } else if (UserInput == 4) {
-                System.out.println("Reverse string has been chosen");
-                ReverseTime = true;
-                break;
+        while (Gaming) {
+            while (SelectionTime) {
+                if (UserInput < 1 || UserInput > 4) {
+                    System.out.println("You have ended the game");
+                    break;
+                } else if (UserInput == 1) {
+                    System.out.println("Find factors has been chosen");
+                    FactorTime = true;
+                    break;
+                } else if (UserInput == 2) {
+                    System.out.println("Find even digits has been chosen");
+                    EvenTime = true;
+                    break;
+                } else if (UserInput == 3) {
+                    System.out.println("Hide vowels has been chosen");
+                    VowelTime = true;
+                    break;
+                } else if (UserInput == 4) {
+                    System.out.println("Reverse string has been chosen");
+                    ReverseTime = true;
+                    break;
+                }
+                System.out.println("\n1. Find Factors\n2. Find even digits");
+                System.out.println("3. Hide vowels\n4. Reverse string");
+                System.out.print("\nChoose a challenge from the list above: ");
+                UserInput = InputReader.nextInt();
             }
-            //initial instructions looped, don't know why Compu wants this
-            System.out.println("\n1. Find Factors\n2. Find even digits");
-            System.out.println("3. Hide vowels\n4. Reverse string");
-            System.out.print("\nChoose a challenge from the list above: ");
-            UserInput = InputReader.nextInt();
-        }
-        while (FactorTime) {
-            //loop section of phase 2
-            System.out.print("\nEnter a number: ");
-            UserInput = InputReader.nextInt();
-
-            //continue here
-        }
-        while (EvenTime) {
-            System.out.print("\nEnter a number:");
-            UserInput = InputReader.nextInt();
-        }
-        while (VowelTime) {
-            System.out.print("\nEnter a vowel:");
-            UserInput = InputReader.nextInt();
-        }
-        while (ReverseTime) {
-            System.out.print("\nEnter a string:");
-            UserString = InputReader.nextLine();
-
-            for (int i = UserString.length(); i > 0; i--) {
-                //start of loop likely required for this section
+            SelectionTime = false;
+            while (FactorTime) { //loop is done
+                System.out.print("\nEnter a number: ");
+                UserInput = InputReader.nextInt();
+                System.out.println("Factors: ");
+                for (int i = 1; i <= UserInput; i++) {
+                    if (UserInput % i == 0) {
+                        System.out.print(i + ", ");
+                    }
+                }
+                FactorTime = false;
             }
+            while (EvenTime) {
+                System.out.print("\nEnter a number:");
+                UserInput = InputReader.nextInt();
+                System.out.println("Even digits: ");
+                while (UserInput > 0) {
+                    int Digit = UserInput % 10;
+                    if (Digit % 2 == 0) {
+                        System.out.print(Digit + ", ");
+                    }
+                    UserInput /= 10;
+                }
+                SelectionTime = true;
+                EvenTime = false;
+            }
+            while (VowelTime) {
+                System.out.print("\nEnter a vowel:");
+                UserString = InputReader.nextLine();
+
+                VowelTime = false;
+            }
+            while (ReverseTime) {
+                System.out.print("\nEnter a string:");
+                UserString = InputReader.nextLine();
+
+
+                for (int i = UserString.length(); i > 0; i--) {
+                    //start of loop likely required for this section
+                }
+                ReverseTime = false;
+            }
+            System.out.println("\n\nChoose another algorithm from the list");
+            UserInput = InputReader.nextInt();
         }
+        System.out.println("\nThank you for playing!");
     }
 }
